fb9cf8a2e0ee6ac3119cc41d3590dc606d414fc9
Meenkshi2k06 <meenaximenon@gmail.com>
2026-06-28 22:18:32 +0530

    commit

---
+++ client/src/routes/AppRoutes.js
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Dashboard from "../pages/Dashboard";
+import Profile from "../pages/Profile";
+import Requests from "../pages/Requests";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/requests" element={<Requests />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
