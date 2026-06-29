f045caf44d07ed2d37682b00666226173a2cc86e
Samera <samerasudeesh216@gmail.com>
2026-06-29 10:48:05 +0530

    resolve merge conflicts

--- server/server.js
+++ server/server.js
@@ -7,12 +7,7 @@ dotenv.config();
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const requestRoutes = require("./routes/requestRoutes");
-<<<<<<< HEAD
-=======
 const userRoutes = require("./routes/userRoutes");
-
-app.use("/api/users", userRoutes);
->>>>>>> 332d848 (Frontend and request feature updates)
 
 connectDB();
 
@@ -22,20 +17,19 @@ app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
-  }),
+  })
 );
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/requests", requestRoutes);
-<<<<<<< HEAD
-=======
 app.use("/api/users", userRoutes);
->>>>>>> 332d848 (Frontend and request feature updates)
+
+app.get("/", (req, res) => {
+  res.send("SkillMate API Running");
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
-});
-app.get("/", (req, res) => {
-  res.send("SkillMate API Running");
 });
